91088ddaf6c76d0c549a72ca982c05d911c9aef6
onetobig <onetobig@gmail.com>
2018-05-30 17:00:58 +0800

    authRequest and logout

--- src/utils/api.js
+++ src/utils/api.js
@@ -60,7 +60,84 @@ const login = async (params = {}) => {
     return authResponse
 }
 
+  // 请求刷新接口
+const refreshToken = async (accessToken) => {
+  let refreshResponse = await wepy.request({
+    url: host + '/' + 'authorizations/current',
+    method: 'PUT',
+    header: {
+      'Authorization': 'Bearer ' + accessToken
+    }
+  })
+
+  if (refreshResponse.statusCode === 200) {
+    wepy.setStorageSync('access_token', refreshResponse.data.access_token)
+    wepy.setStorageSync('access_token_expired_at', new Date().getTime() + refreshResponse.data.expires_in * 1000)
+  }
+
+  return refreshResponse;
+}
+
+// 获取 Token
+const getToken = async (options) => {
+  // 从缓存中取出 token
+  let accessToken = wepy.getStorageSync('access_token')
+  let expiredAt = wepy.getStorageSync('access_token_expired_at')
+
+  // 过期调用刷新方法
+  if (accessToken && new Date().getTime() > expiredAt) {
+    let refreshResponse = await refreshToken(accessToken)
+
+    // 刷新成功
+    if (refreshResponse.statusCode === 200) {
+      accessToken = refreshResponse.data.access_token
+    } else {
+      // 刷新失败
+      let authResponse = await login()
+      if (authResponse.statusCode === 201) {
+        accessToken = authResponse.data.access_token
+      }
+    }
+  }
+  return accessToken
+}
+
+//  带身份认证的请求
+const authRequest = async (options, showLoading = true) => {
+  if (typeof options === 'string') {
+    options = {
+      url: options
+    }
+  }
+
+  let accessToken = await getToken();
+
+  let header = options.header || {}
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+  return request(options, showLoading)
+}
+
+const logout = async (params = {}) => {
+  let accessToken = wepy.getStorageSync('access_token')
+
+  // 调用删除 Token 接口
+  let logoutResponse = await authRequest({
+    url: 'authorizations/current',
+    method: 'DELETE'
+  })
+  // 调用接口成功则清空缓存
+  if (logoutResponse.statusCode === 204) {
+    wepy.clearStorage()
+  }
+  return logoutResponse
+}
+
 export default {
     request,
-    login
+    login,
+    refreshToken,
+    getToken,
+    authRequest,
+    logout
 }
