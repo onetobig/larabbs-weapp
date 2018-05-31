f5d851f8eb112500c8c3ea04c8933d2af21c6b4b
onetobig <onetobig@gmail.com>
2018-05-31 15:46:25 +0800

    update avatar

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,63 +4,63 @@ const host = 'http://larabbs.test/api'
 
 // 普通请求
 const request = async (options, showLoading = true) => {
-    if (typeof options === 'string') {
-        options = {
-            url: options
-        }
+  if (typeof options === 'string') {
+    options = {
+      url: options
     }
+  }
 
-    // 显示加载中
-    if (showLoading) {
-        wepy.showLoading({title: '加载中'})
-    }
+  // 显示加载中
+  if (showLoading) {
+    wepy.showLoading({title: '加载中'})
+  }
 
-    // 拼接请求地址
-    options.url = host + '/' + options.url
-    // 调用小程序的 request 方法
-    let response = await wepy.request(options)
+  // 拼接请求地址
+  options.url = host + '/' + options.url
+  // 调用小程序的 request 方法
+  let response = await wepy.request(options)
 
-    if (showLoading) {
-        // 隐藏加载中
-        wepy.hideLoading()
-    }
+  if (showLoading) {
+    // 隐藏加载中
+    wepy.hideLoading()
+  }
 
-    // 服务器异常后给予提示
-    if (response.statusCode === 500) {
-        wepy.showModel({
-            title:  '提示',
-            content: '服务器错误，请联系管理员或重试'
-        })
-    }
+  // 服务器异常后给予提示
+  if (response.statusCode === 500) {
+    wepy.showModel({
+      title: '提示',
+      content: '服务器错误，请联系管理员或重试'
+    })
+  }
 
-    return response
+  return response
 }
 
 // 登录
 const login = async (params = {}) => {
-    // code 只能使用一次，所以每次单独调用
-    let loginData = await wepy.login()
+  // code 只能使用一次，所以每次单独调用
+  let loginData = await wepy.login()
 
-    // 参数中增加code
-    params.code = loginData.code
+  // 参数中增加code
+  params.code = loginData.code
 
-    // 接口请求 weapp/authorizations
-    let authResponse = await request({
-        url: 'weapp/authorizations',
-        data: params,
-        method: 'POST'
-    })
+  // 接口请求 weapp/authorizations
+  let authResponse = await request({
+    url: 'weapp/authorizations',
+    data: params,
+    method: 'POST'
+  })
 
-    // 登录成功，记录 token 信息
-    if (authResponse.statusCode === 201) {
-        wepy.setStorageSync('access_token', authResponse.data.access_token)
-        wepy.setStorageSync('access_token_expired_at', new Date().getTime() + authResponse.data.expires_in * 1000)
-    }
+  // 登录成功，记录 token 信息
+  if (authResponse.statusCode === 201) {
+    wepy.setStorageSync('access_token', authResponse.data.access_token)
+    wepy.setStorageSync('access_token_expired_at', new Date().getTime() + authResponse.data.expires_in * 1000)
+  }
 
-    return authResponse
+  return authResponse
 }
 
-  // 请求刷新接口
+// 请求刷新接口
 const refreshToken = async (accessToken) => {
   let refreshResponse = await wepy.request({
     url: host + '/' + 'authorizations/current',
@@ -141,12 +141,32 @@ const showErrorModal = async (err) => {
   })
 }
 
+const uploadFile = async (options = {}) => {
+  wepy.showLoading({title: '上传中'})
+
+  let accessToken = await getToken()
+
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+  header.Authorization = 'Bearer ' + accessToken
+  options.header = header
+
+  // 上传文件
+  let response = await wepy.uploadFile(options)
+
+  // 隐藏 loading
+  wepy.hideLoading()
+
+  return response
+}
+
 export default {
-    request,
-    login,
-    refreshToken,
-    getToken,
-    authRequest,
-    logout,
-    showErrorModal
+  request,
+  login,
+  refreshToken,
+  getToken,
+  authRequest,
+  logout,
+  showErrorModal,
+  uploadFile
 }
